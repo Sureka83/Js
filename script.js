2e2de3d69bf57608aebcde57fda7eb065b49f637
Sureka83 <surekamariappan@gmail.com>
2025-09-19 09:30:08 +0530

    JS

---
+++ script.js
@@ -0,0 +1,29 @@
+// Condition
+var num=541878451657654
+if (num%2==0) {
+    console.log("The number is an Even Number")
+} else {
+    console.log("The number is an Odd Number")
+}
+// Operators
+var a=544
+var b=++a
+var a=b--
+var b=--a
+var a=b++
+console.log("The value of a is",a)
+console.log("The value of b is",b)
+// ex
+var ab=100
+var bc=--ab
+console.log(ab)
+console.log(bc)
+console.log(ab)
+pdata={
+    fname:"Sureka",
+    Age:19,
+    DOB:19/2/2006,
+    Gender:Female,
+    RegisterNumber:950623104098,
+    Department:
+}
