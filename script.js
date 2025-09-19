26e19b1b7439000b8d345d0331ef906091bb674a
Sureka83 <surekamariappan@gmail.com>
2025-09-19 10:15:50 +0530

    JS

--- script.js
+++ script.js
@@ -19,12 +19,28 @@
 /* console.log(ab) */
 /* console.log(bc) */
 /* console.log(ab) */
-pdata={
-    fname:"Sureka",
-    Age:19,
-    DOB:19/2/2006,
-    Gender:"Female",
-    RegisterNumber:950623104098,
-    Department:"CSE",
+/* pdata={ */
+/*     fname:"Sureka", */
+/*     Age:19, */
+/*     DOB:19/2/2006, */
+/*     Gender:"Female", */
+/*     RegisterNumber:950623104098, */
+/*     Department:"CSE", */
+/* } */
+/* console.log(pdata) */
+function mark(m1,m2,m3){
+    result=(m1+m2+m3)/3
+    if(result>=90)
+        console.log("Grade A")
+    else if(result>=80)
+        console.log("Grade B")
+    else if(result>=70)
+        console.log("Grade C")
+    else if(result>=60)
+        console.log("Grade D")
+    else if(result>=50)
+        console.log("Grade E")
+    else 
+        console.log("Fail")
 }
-console.log(pdata)
+mark(87,90,98)
